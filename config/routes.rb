390724641e569046378c7a9e74bbe0d9887b6355
StuartAhmet <sma0190@gmail.com>
2022-11-01 16:28:13 +0000

    updated routes - devise users - now the registration path is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'user/registrations',
-    sessions: 'users/sessions',
-    omniauth_callbacks: 'users/omniauth_callbacks'
-    }
+  devise_for :users
+  # controllers: {
+  #   registrations: 'user/registrations',
+  #   sessions: 'users/sessions',
+  #   omniauth_callbacks: 'users/omniauth_callbacks'
+  #   }
 
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
